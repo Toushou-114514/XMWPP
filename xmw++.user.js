ba2cbae67ef514629d1170f095ecba5b26108119
Toushou <ii45i41oo86@163.com>
2026-09-21 22:53:33 +0800

    文件
    Added update and download URLs for the user script.

--- xmw++.user.js
+++ xmw++.user.js
@@ -10,6 +10,8 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @updateURL    https://raw.githubusercontent.com/Toushou-114514/XMWPP/main/XMW++.user.js
+// @downloadURL  https://raw.githubusercontent.com/Toushou-114514/XMWPP/main/XMW++.user.js
 // ==/UserScript==
 
 (function() {
